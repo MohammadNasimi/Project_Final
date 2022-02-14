7ebcaa5014fc357a7331c3eefcdea7431627741f
mohammad <mohammadnasimi6803@gmail.com>
2022-02-14 23:31:35 +0330

    add/Order_items models

--- order/models.py
+++ order/models.py
@@ -10,4 +10,7 @@ class Of_code(BaseDiscount):
     off_code = models.CharField(max_length=10, unique=True)
 
 
-
+class Order_items(BaseModel):
+    Product = models.ForeignKey(Product, on_delete=models.RESTRICT, null=True, blank=True)
+    Count = models.PositiveIntegerField(null=True, blank=True, default=1)
+    Price_product = models.PositiveIntegerField(null=True, default=0)
