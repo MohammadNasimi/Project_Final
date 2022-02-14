b74c071f90a96899f493678be2a0a56393c7f404
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 01:13:19 +0330

    add/Order_status attr

--- order/models.py
+++ order/models.py
@@ -3,6 +3,8 @@ from core.models import BaseModel, BaseDiscount
 from product.models import Product
 from customer.models import Customer
 
+from model_utils import Choices
+
 
 # Create your models here.
 
@@ -11,19 +13,23 @@ class Of_code(BaseDiscount):
     off_code = models.CharField(max_length=10, unique=True)
 
 
-class Order_items(BaseModel):
+class Order_item(BaseModel):
     Product = models.ForeignKey(Product, on_delete=models.RESTRICT, null=True, blank=True)
     Count = models.PositiveIntegerField(null=True, blank=True, default=1)
     Price_product = models.PositiveIntegerField(null=True, default=0)
 
 
-status = (('Current', 1), ('Delivered', 2), ('Canceled', 3))
+ORDER_STATUS = Choices(
+    (1, 'Current', 'Current'),
+    (2, 'Delivered', 'Delivered'),
+    (3, 'Canceled', 'Canceled')
+)
 
 
 class Order(BaseModel):
     customer = models.ForeignKey(Customer, on_delete=models.RESTRICT, null=True, blank=True)
-    order_items = models.ManyToManyField(Order_items, on_delete=models.RESTRICT, null=True, blank=True)
-    status_Order = models.PositiveIntegerField(choices=status, default=1, null=True, blank=True)
+    order_items = models.ManyToManyField(Order_item)
+    status_Order = models.IntegerField(choices=ORDER_STATUS, default=1, null=True, blank=True)
     date = models.DateTimeField()
     price_all = models.PositiveIntegerField()
     off_code = models.ForeignKey(Of_code, on_delete=models.RESTRICT)
