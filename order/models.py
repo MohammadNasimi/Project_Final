d00ce1ab661be7e68e80ae72eec9bb80784d0e43
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 00:49:01 +0330

    add/Order(cart) models

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.models import BaseModel, BaseDiscount
 from product.models import Product
+from customer.models import Customer
 
 
 # Create your models here.
@@ -14,3 +15,15 @@ class Order_items(BaseModel):
     Product = models.ForeignKey(Product, on_delete=models.RESTRICT, null=True, blank=True)
     Count = models.PositiveIntegerField(null=True, blank=True, default=1)
     Price_product = models.PositiveIntegerField(null=True, default=0)
+
+
+status = (('Current', 1), ('Delivered', 2), ('Canceled', 3))
+
+
+class Order(BaseModel):
+    customer = models.ForeignKey(Customer, on_delete=models.RESTRICT, null=True, blank=True)
+    order_items = models.ManyToManyField(Order_items, on_delete=models.RESTRICT, null=True, blank=True)
+    status_Order = models.PositiveIntegerField(choices=status, default=1, null=True, blank=True)
+    date = models.DateTimeField()
+    price_all = models.PositiveIntegerField()
+    off_code = models.ForeignKey(Of_code, on_delete=models.RESTRICT)
