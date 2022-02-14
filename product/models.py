aa01819dccebf36021466e8de19edb557dda5cef
mohammad <mohammadnasimi6803@gmail.com>
2022-02-14 23:11:36 +0330

    change/product attr name

--- product/models.py
+++ product/models.py
@@ -14,12 +14,12 @@ class Category(BaseModel):
 
 
 class Product(BaseModel):
-    Name_product = models.CharField(max_length=20, null=True, blank=True)
+    name_product = models.CharField(max_length=20, null=True, blank=True)
     brand = models.CharField(max_length=20, null=False)
-    descriptions = models.CharField(max_length=250, null=False)
-    price = models.IntegerField()
-    number_store = models.IntegerField(default=0)
-    Status = models.CharField(max_length=15, choices=(('exist', True), ('Not exist', False)), default=False)
+    descriptions = models.TextField(max_length=250, null=False)
+    price = models.PositiveIntegerField()
+    number_store = models.PositiveIntegerField(default=0)
+    status = models.CharField(max_length=15, choices=(('exist', True), ('Not exist', False)), default=False)
     category = models.ForeignKey(Category, on_delete=models.RESTRICT, null=True, blank=True)
     image = models.FileField(null=True, default=None, upload_to='Product_pic/%Y/%m/%d', blank=True)
     discount = models.ForeignKey(Discount, on_delete=models.RESTRICT, null=False)
