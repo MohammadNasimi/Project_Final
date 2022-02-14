f934276b65d622971411953d8085889e6a368c22
mohammad <mohammadnasimi6803@gmail.com>
2022-02-14 04:41:00 +0330

    add/static for file upload

--- Project_Final/urls.py
+++ Project_Final/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
     prefix_default_language=True
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
