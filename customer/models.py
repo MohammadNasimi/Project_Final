d802816b8f2709a53d3a2854eafbe6d4a6725a8e
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 00:43:48 +0330

    create/model Address and Customer

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+from core.models import BaseModel
+
+
+class Address(BaseModel):
+    province = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=20, null=True, blank=True)
+    town = models.CharField(max_length=20, null=True, blank=True)
+    street = models.CharField(max_length=20, null=True, blank=True)
+    alley = models.CharField(max_length=20, null=True, blank=True)
+    Plaque = models.IntegerField(unique=True, null=True, blank=True)
+    zip_code = models.IntegerField(unique=True, null=True, blank=True)
+
+
+class Customer(User):
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    address = models.ForeignKey(Address, on_delete=models.RESTRICT)
