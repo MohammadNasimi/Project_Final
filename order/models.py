9926edf873675c666cd346fce76f5b64bd000fdc
mohammad <mohammadnasimi6803@gmail.com>
2022-02-14 23:30:59 +0330

    add/Off_code models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import BaseModel, BaseDiscount
+from product.models import Product
+
 
 # Create your models here.
+
+
+class Of_code(BaseDiscount):
+    off_code = models.CharField(max_length=10, unique=True)
+
+
+
