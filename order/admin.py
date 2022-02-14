189598c4d6b4336fe435ca71a628a4fb28beba44
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 01:14:15 +0330

    add/Order app admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from order.models import Order_item, Of_code, Order
 
 # Register your models here.
+admin.site.register(Of_code)
+admin.site.register(Order_item)
+admin.site.register(Order)
