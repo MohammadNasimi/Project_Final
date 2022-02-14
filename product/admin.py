a9adc60ad5a14d7e352091f3d7a7fd6265b04b2e
mohammad <mohammadnasimi6803@gmail.com>
2022-02-14 23:00:40 +0330

    add/Discount,Category,Product in admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from product.models import Discount, Category, Product
 
 # Register your models here.
+admin.site.register(Discount)
+admin.site.register(Category)
+admin.site.register(Product)
