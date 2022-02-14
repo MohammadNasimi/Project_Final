8994b2025ce22a0dc3ff7fe8e4f50d3d8f389f5d
mohammad <mohammadnasimi6803@gmail.com>
2022-02-14 22:57:08 +0330

    add/Product models

--- product/models.py
+++ product/models.py
@@ -6,11 +6,20 @@ from core.models import BaseModel, BaseDiscount
 class Discount(BaseDiscount):
     pass
 
+
 class Category(BaseModel):
     category_name = models.CharField(max_length=20, null=True, blank=True)
     category_root = models.ForeignKey('self', on_delete=models.RESTRICT, null=False)
     discount = models.ForeignKey(Discount, on_delete=models.RESTRICT, null=False)
 
 
-
-
+class Product(BaseModel):
+    Name_product = models.CharField(max_length=20, null=True, blank=True)
+    brand = models.CharField(max_length=20, null=False)
+    descriptions = models.CharField(max_length=250, null=False)
+    price = models.IntegerField()
+    number_store = models.IntegerField(default=0)
+    Status = models.CharField(max_length=15, choices=(('exist', True), ('Not exist', False)), default=False)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, null=True, blank=True)
+    image = models.FileField(null=True, default=None, upload_to='Product_pic/%Y/%m/%d', blank=True)
+    discount = models.ForeignKey(Discount, on_delete=models.RESTRICT, null=False)
