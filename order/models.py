662bd89545be0b9187b2005cd6ae826e8f6e6c8c
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 17:54:07 +0330

    add/str method in order

--- order/models.py
+++ order/models.py
@@ -12,10 +12,16 @@ from model_utils import Choices
 class Of_code(BaseDiscount):
     off_code = models.CharField(max_length=10, unique=True)
 
+    def __str__(self):
+        return f'{self.type}:{self.value}'
+
 
 class Order_item(BaseModel):
     Product = models.ForeignKey(Product, on_delete=models.RESTRICT, null=False)
     Count = models.PositiveIntegerField(null=True, blank=True, default=1)
+
+    def __str__(self):
+        return f'{self.Product}:{self.Count}'
 
     @property
     def get_cost_end_product(self):
@@ -35,5 +41,11 @@ class Order(BaseModel):
     order_items = models.ManyToManyField(Order_item)
     status_Order = models.IntegerField(choices=ORDER_STATUS, default=1, null=True, blank=True)
     date = models.DateTimeField()
-    price_all = models.PositiveIntegerField()
     off_code = models.ForeignKey(Of_code, on_delete=models.RESTRICT)
+
+    @property
+    def get_total_cost(self):
+        return sum(item.get_cost_end_product() for item in self.order_items.all())
+
+    def __str__(self):
+        return f'{self.customer}:{self.order_items}'
