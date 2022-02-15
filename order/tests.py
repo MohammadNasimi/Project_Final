da89d14c77f751fcd7809b2c31976a52c8b9879d
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 20:07:35 +0330

    add/test_get_all_order_items method in Order

--- order/tests.py
+++ order/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from product.models import Discount, Product, Category
 from order.models import Order_item, Order, Of_code
+from customer.models import Customer
 
 
 # Create your tests here.
@@ -31,8 +32,31 @@ class OrderTestcase(TestCase):
         self.order_item1 = Order_item.objects.create(Product=self.product1, Count=10)
         self.order_item2 = Order_item.objects.create(Product=self.product2, Count=1)
         self.order_item3 = Order_item.objects.create(Product=self.product3, Count=2)
+        # create model of customer
+        self.customer1 = Customer.objects.create(username='ahmad20', password='123456')
+        # create model of Of_code
+        self.off_code1 = Of_code.objects.create(value=0, type='price', off_code='123456')
+        # create model of Order
+        self.Order1 = Order.objects.create(customer=self.customer1, off_code=self.off_code1)
+        self.Order1.order_items.add(self.order_item1, self.order_item2, self.order_item3)
+        self.Order1.save()
+        self.Order2 = Order.objects.create(customer=self.customer1, off_code=self.off_code1)
+        self.Order2.order_items.add(self.order_item2, self.order_item3)
+        self.Order2.save()
 
     def test_get_cost_end_product(self):
         self.assertEqual(self.order_item1.get_cost_end_product, 220000)
         self.assertEqual(self.order_item2.get_cost_end_product, 228450000)
         self.assertEqual(self.order_item3.get_cost_end_product, 319900000)
+
+    def test_get_total_cost(self):
+        # print(self.Order1.order_items.all())
+        # print(self.Order2.order_items.all())
+        # print(self.Order2.get_all_order_items)
+        # print(self.Order2.objects.filter(order_items__Count=10))
+        self.assertEqual(self.Order1.get_total_cost, 548570000)
+        self.assertEqual(self.Order2.get_total_cost, 548350000)
+
+    def test_get_all_order_items(self):
+        self.assertEqual(self.Order1.get_all_order_items, ['Pizza peperoni', 'Samand', 'perayd'])
+        self.assertEqual(self.Order2.get_all_order_items, ['Samand', 'perayd'])
