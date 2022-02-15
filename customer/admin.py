867c5d5b5d2d2cc80f19de97f889ec11832f85bc
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 22:01:35 +0330

    add/setting admin for app customer

--- customer/admin.py
+++ customer/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from customer.models import Address, Customer
 
+
 # Register your models here.
-admin.site.register(Address)
-admin.site.register(Customer)
+class AddressAdmin(admin.ModelAdmin):
+    fields = ['province', 'city', 'town', 'street', 'alley', 'Plaque', 'zip_code']
+    list_display = ['province', 'city', 'town']
+    list_filter = ['province', 'city', 'town']
+    search_fields = ['province', 'city', 'town', 'zip_code']
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Customer, CustomerAdmin)
