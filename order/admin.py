6b3fc88885306e020ae07b979311080dcfa25422
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 21:54:22 +0330

    add/setting admin for app order

--- order/admin.py
+++ order/admin.py
@@ -2,6 +2,37 @@ from django.contrib import admin
 from order.models import Order_item, Of_code, Order
 
 # Register your models here.
-admin.site.register(Of_code)
-admin.site.register(Order_item)
-admin.site.register(Order)
+from django.contrib import admin
+from product.models import Discount, Category, Product
+from django.contrib import admin
+
+
+# Register your models here.
+class Of_codeAdmin(admin.ModelAdmin):
+    fields = ['value', 'type', 'max_price', 'off_code']
+    list_display = ['value', 'type']
+    list_filter = ['value', 'type']
+    search_fields = ['value', 'type', 'off_code']
+
+
+class Order_itemAdmin(admin.ModelAdmin):
+    fields = ['Product', 'Count']
+    list_display = ['Product']
+    list_filter = ['Product', 'Count']
+    search_fields = ['Product', 'Count']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('General',
+         {'fields': ['customer', 'status_Order', 'date']}),
+        ('order items', {'fields': ['order_items']}),
+        ('off_code', {'fields': ['off_code']})
+    )
+    list_filter = ['customer', 'date']
+    search_fields = ['customer', 'date', 'price']
+
+
+admin.site.register(Of_code, Of_codeAdmin)
+admin.site.register(Order_item, Order_itemAdmin)
+admin.site.register(Order, OrderAdmin)
