87f16632a0f7b3891675b69d94e5e3ef3041a8b6
mohammad <mohammadnasimi6803@gmail.com>
2022-02-15 17:18:07 +0330

    add/get_cost_all method for Order_item

--- order/tests.py
+++ order/tests.py
@@ -7,10 +7,10 @@ from order.models import Order_item, Order, Of_code
 class OrderTestcase(TestCase):
     def setUp(self):
         # create model for product
-
         self.discount1 = Discount.objects.create(value=20, type='percent', max_price='50000')
         self.discount2 = Discount.objects.create(value=2000, type='price')
         self.discount3 = Discount.objects.create(value=1500000, type='price')
+        self.discount4 = Discount.objects.create(value=0, type='price')
         self.category1 = Category.objects.create(category_name='Food')
         self.category2 = Category.objects.create(category_name='Car', discount=self.discount1)
         self.category3 = Category.objects.create(category_name='Pizza', category_root=self.category1,
@@ -26,13 +26,13 @@ class OrderTestcase(TestCase):
         self.product3 = Product.objects.create(name_product='perayd', brand='Iran',
                                                descriptions='Made in Iran', price=160000000, status=True,
                                                number_store=2,
-                                               category=self.category2)
+                                               category=self.category2, discount=self.discount4)
         # create model of order_items
         self.order_item1 = Order_item.objects.create(Product=self.product1, Count=10)
         self.order_item2 = Order_item.objects.create(Product=self.product2, Count=1)
         self.order_item3 = Order_item.objects.create(Product=self.product3, Count=2)
 
     def test_get_cost_end_product(self):
-        self.assertEqual(self.order_item1.get_cost_end_product, 248000)
+        self.assertEqual(self.order_item1.get_cost_end_product, 220000)
         self.assertEqual(self.order_item2.get_cost_end_product, 228450000)
-        self.assertEqual(self.order_item3.get_cost_end_product, 248000)
+        self.assertEqual(self.order_item3.get_cost_end_product, 319900000)
