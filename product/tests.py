eb0189a8fdf1b4354a5fc6aa2bc08b0367cf67f6
mohammad <mohammadnasimi6803@gmail.com>
2022-02-16 19:04:31 +0330

    add/test product

--- product/tests.py
+++ product/tests.py
@@ -10,6 +10,7 @@ class CategoryTestcase(TestCase):
         self.discount1 = Discount.objects.create(value=20, type='percent', max_price='1000000')
         self.discount2 = Discount.objects.create(value=2000, type='price')
         self.discount3 = Discount.objects.create(value=1500000, type='price')
+        self.discount4 = Discount.objects.create(value=0, type='price')
         self.category1 = Category.objects.create(category_name='Food')
         self.category2 = Category.objects.create(category_name='Car', discount=self.discount1)
         self.category3 = Category.objects.create(category_name='Pizza', category_root=self.category1,
@@ -25,7 +26,7 @@ class CategoryTestcase(TestCase):
         self.product3 = Product.objects.create(name_product='perayd', brand='Iran',
                                                descriptions='Made in Iran', price=160000000, status=True,
                                                number_store=2,
-                                               category=self.category2)
+                                               category=self.category2,discount=self.discount4)
 
     def test_profit_value(self):
         self.assertEqual(self.discount1.profit_value(10000), 2000)
@@ -34,4 +35,3 @@ class CategoryTestcase(TestCase):
         self.assertEqual(self.product1.discount.profit_value(30000), 6000)
         self.assertEqual(self.product2.discount.profit_value(230000000), 1500000)
         self.assertEqual(self.product3.category.discount.profit_value(160000000), 1000000)
-
