52b93ad13a1fdd2f6455224c5f4a3dda892222ca
mohammad <mohammadnasimi6803@gmail.com>
2022-02-18 23:10:32 +0330

    add/logout url

--- landing/urls.py
+++ landing/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import HomeView, LoginView, registerView
+from .views import HomeView, LoginView, registerView, LogoutView
 
 app_name = 'landing'
 urlpatterns = [
     path('', HomeView.as_view()),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', registerView.as_view(), name='register'),
-
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
