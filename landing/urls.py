d1b7605d2e767063cad6c9f828c1da069e03215b
mohammad <mohammadnasimi6803@gmail.com>
2022-02-18 21:40:00 +0330

    add/register url

--- landing/urls.py
+++ landing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomeView, LoginView
+from .views import HomeView, LoginView, registerView
 
 app_name = 'landing'
 urlpatterns = [
     path('', HomeView.as_view()),
     path('login/', LoginView.as_view(), name='login'),
+    path('register/', registerView.as_view(), name='register'),
 
 ]
