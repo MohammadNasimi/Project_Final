875731d329bcd334e9ddb789df9ddd0e32760f14
mohammad <mohammadnasimi6803@gmail.com>
2022-02-18 23:46:27 +0330

    add/home url name=Home

--- landing/urls.py
+++ landing/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, LoginView, registerView, LogoutView
 
 app_name = 'landing'
 urlpatterns = [
-    path('', HomeView.as_view()),
+    path('', HomeView.as_view(),name ='Home'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', registerView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
