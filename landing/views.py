3a3c677c86f485068ae957af98ae94efccd16a38
mohammad <mohammadnasimi6803@gmail.com>
2022-02-18 19:55:46 +0330

    add/login  View

--- landing/views.py
+++ landing/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.views import View
 
 
 class HomeView(TemplateView):
     template_name = "landing/base/_base.html"
+
+
+class LoginView(View):
+
+    def get(self, request):
+        return render(request, 'landing/public/Login.html')
