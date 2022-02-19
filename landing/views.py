603f38ca39eab897d32ffd97dbd630a092a228f7
mohammad <mohammadnasimi6803@gmail.com>
2022-02-20 00:36:42 +0330

    add/permission when user register

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Permission, ContentType
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -6,6 +8,7 @@ from django.views import View
 
 from core.models import User
 from customer.forms import CustomerForm
+from customer.models import Customer
 
 
 class HomeView(TemplateView):
@@ -20,10 +23,30 @@ class registerView(View):
         return render(request, 'landing/public/register.html', {'form': self.form_class})
 
     def post(self, request):
-        ...
+        phone = request.POST['phone']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password = request.POST['password1']
+        new_user = User.objects.create_user(username=phone, email=email,
+                                            first_name=first_name, last_name=last_name, password=password, phone=phone)
+        Customer.objects.create(user=new_user)
+        # code add permission see-profile in terminal
+        # content_type = ContentType.objects.get_for_model(User)
+        # permission = Permission.objects.create(
+        #     name='can see profile',
+        #     codename='see_profile',
+        #     content_type=content_type
+        # )
+        # add permission when register
+        content_type = ContentType.objects.get_for_model(User)
+        permission = Permission.objects.get(content_type=content_type, codename='see_profile')
+        new_user.user_permissions.add(permission)
+        return render(request, 'landing/public/Login.html')
 
 
-class LoginView(View):
+class LoginView(PermissionRequiredMixin, View):
+    permission_required = "auth.see_profile"
 
     def get(self, request):
         if request.session.get('uid', None) is None:
@@ -33,6 +56,7 @@ class LoginView(View):
             'user': user
 
         }
+
         return render(request, 'landing/public/profile.html', context=user)
 
     def post(self, request):
