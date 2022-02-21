6811fb669a881b731ebe64026c04b5ed4144bff6
mohammad <mohammadnasimi6803@gmail.com>
2022-02-21 11:15:38 +0330

    add/profile url

--- landing/urls.py
+++ landing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, LoginView, registerView, LogoutView
+from .views import HomeView, LoginView, registerView, LogoutView,profileView
 
 app_name = 'landing'
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', registerView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', profileView.as_view(), name='profile'),
 ]
