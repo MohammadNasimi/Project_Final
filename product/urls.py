9a9130f1dd061fa9062701a0860c6d60746230d4
mohammad <mohammadnasimi6803@gmail.com>
2022-02-24 00:16:03 +0330

    remove/category  url

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import CategoryListview, product_for_categoryListview
+from .views import product_for_categoryListview
 
 app_name = 'product'
 urlpatterns = [
-    path('Category/', CategoryListview.as_view(), name='category'),
+    # path('Category/', CategoryListview.as_view(), name='category'),
     # path('product/', productListview.as_view(), name='product'),
     path('category_product/<int:pk>', product_for_categoryListview.as_view(), name='category_product'),
 ]
