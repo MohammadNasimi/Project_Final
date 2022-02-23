81a911a4f5066e3f8ba419f094f47895fe9fa4ba
mohammad <mohammadnasimi6803@gmail.com>
2022-02-24 00:53:22 +0330

    add/detail product

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import product_for_categoryListview
+from .views import product_for_categoryListview, ProductDetailView
 
 app_name = 'product'
 urlpatterns = [
     # path('Category/', CategoryListview.as_view(), name='category'),
     # path('product/', productListview.as_view(), name='product'),
     path('category_product/<int:pk>', product_for_categoryListview.as_view(), name='category_product'),
+    path('Detail_product/<int:pk>', ProductDetailView.as_view(), name='Detail_product'),
 ]
