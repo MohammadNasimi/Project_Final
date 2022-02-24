59eb666cf56235d4605ce939b708f446148fa83d
mohammad <mohammadnasimi6803@gmail.com>
2022-02-24 22:19:28 +0330

    add/serializers customer and Address

---
+++ customer/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from customer.models import Address
+from core.models import User
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
