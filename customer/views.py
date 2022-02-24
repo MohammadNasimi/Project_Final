87d98133012512f70203a2dc6e66270bba792e36
mohammad <mohammadnasimi6803@gmail.com>
2022-02-24 23:15:35 +0330

    add/authentication_classes for views

--- customer/views.py
+++ customer/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from customer.permissions import IsOwnerPermission, IsSuperuserPermission
 from customer.serializers import AddressSerializer, CustomerSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions ,authentication
 from core.models import User
 from customer.models import Address
 
@@ -12,18 +12,21 @@ class UserlistViewApi(generics.ListAPIView):
     serializer_class = CustomerSerializer
     queryset = User.objects.all()
     permission_classes = [permissions.IsAdminUser, IsSuperuserPermission]
+    authentication_classes = [authentication.BasicAuthentication]
 
 
 class UserDetailViewApi(generics.RetrieveAPIView):
     serializer_class = CustomerSerializer
     queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticated, IsSuperuserPermission]
+    # authentication_classes = [authentication.BasicAuthentication]
 
 
 class AddresslistViewApi(generics.ListAPIView):
     serializer_class = AddressSerializer
     queryset = Address.objects.all()
     permission_classes = [permissions.IsAuthenticated, IsOwnerPermission]
+    # authentication_classes = [authentication.BasicAuthentication]
 
     def get_queryset(self):
         return Address.objects.filter(customer__user=self.request.user)
@@ -33,3 +36,4 @@ class AddressDetailViewApi(generics.RetrieveAPIView):
     serializer_class = AddressSerializer
     queryset = Address.objects.all()
     permission_classes = [permissions.IsAuthenticated, IsOwnerPermission]
+    # authentication_classes = [authentication.BasicAuthentication]
