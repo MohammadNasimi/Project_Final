1cc6ff553f6c3d75901ae07268ca35597add9b16
mohammad <mohammadnasimi6803@gmail.com>
2022-02-26 00:05:11 +0330

    add/ serializers app order with hyper link

--- order/serializers.py
+++ order/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 
+from customer.serializers import AddressSerializer
+from product.serializers import ProductSerializer
 from order.models import Order, Order_item
+
+
+class Order_itemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order_item
+        fields = '__all__'
+
+    Product = ProductSerializer(read_only=True)
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -8,8 +18,6 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = '__all__'
 
-
-class Order_itemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order_item
-        fields = '__all__'
+    address = AddressSerializer(read_only=True)
+    # order_item_set = serializers.HyperlinkedRelatedField(view_name='order:Order_item',
+    #                                                       queryset=Order_item.objects.all(), many=True)
