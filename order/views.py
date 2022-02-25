eccefc05d7434452fd1cf4075821a13c093baa8d
mohammad <mohammadnasimi6803@gmail.com>
2022-02-25 23:26:11 +0330

    add/Order Order_item class view with viewsets

--- order/views.py
+++ order/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from order.models import Order,Order_item
+from order.serializers import OrderSerializer, Order_itemSerializer
+
+
+class OrderViewSets(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class Order_itemViewSets(viewsets.ModelViewSet):
+    queryset = Order_item.objects.all()
+    serializer_class = Order_itemSerializer
