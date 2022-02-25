3ceadccb357690dde99e5e927bc6b2193b916d57
mohammad <mohammadnasimi6803@gmail.com>
2022-02-25 21:23:26 +0330

    change/User to Customer in permissions

--- customer/permissions.py
+++ customer/permissions.py
@@ -7,11 +7,11 @@ class IsSuperuserPermission(BasePermission):
         return request.user.is_superuser
 
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.customer.user
+        return request.user == obj.user
 
 
 class IsOwnerPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        # print('user use', obj.user)
-        # print('user', request.user)
-        return request.user == obj.user
+        # print('user use', request.user)
+        # print('user', obj.customer.user)
+        return request.user == obj.customer.user
