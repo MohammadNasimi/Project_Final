ac4d7b2ee0d558d29d42b3f9d523a84a8388855b
mohammad <mohammadnasimi6803@gmail.com>
2022-02-26 00:05:51 +0330

    add/ view app order with template for OrderViewSets

--- order/views.py
+++ order/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, renderers
+from rest_framework import permissions
 
 # Create your views here.
-from order.models import Order,Order_item
+from order.models import Order, Order_item
 from order.serializers import OrderSerializer, Order_itemSerializer
+
+from customer.permissions import IsOwnerPermission, IsSuperuserPermission
 
 
 class OrderViewSets(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsOwnerPermission, permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(address__customer__user=self.request.user)
+
+    renderer_classes = [renderers.JSONRenderer, renderers.TemplateHTMLRenderer]
+    template_name = 'landing/order/order_customer.html'
 
 
 class Order_itemViewSets(viewsets.ModelViewSet):
