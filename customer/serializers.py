90d934d7f2049074d3ec4945933c98dd30b42116
mohammad <mohammadnasimi6803@gmail.com>
2022-02-25 20:17:39 +0330

    change/User to Customer in serializer

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 
-from customer.models import Address
+from customer.models import Address,Customer
 from core.models import User
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['user', 'address_set']
+    address_set = serializers.HyperlinkedRelatedField(view_name='customer:Address_Detail',queryset=Address.objects.all(), many=True)
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -9,8 +16,4 @@ class AddressSerializer(serializers.ModelSerializer):
         model = Address
         fields = '__all__'
 
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
+    customer = CustomerSerializer(read_only=True)
