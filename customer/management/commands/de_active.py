cb91e2719cfa6627bf34167ce3ae6f9f1c98a611
mohammad <mohammadnasimi6803@gmail.com>
2022-02-25 10:39:30 +0330

    add/command is and de active with try and except

--- customer/management/commands/de_active.py
+++ customer/management/commands/de_active.py
@@ -1,16 +1,18 @@
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 from argparse import ArgumentParser
 from core.models import User
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
+    def add_arguments(self, parser: ArgumentParser):
         parser.add_argument('-p', '--phone', help='please enter your phone number')
 
     def handle(self, *args, **options):
         phone = options['phone']
-        user = User.objects.get(phone=phone)
-        user.is_active = False
-        user.save()
-        print(f'de_active {user.is_active}')
-
+        try:
+            user = User.objects.get(phone=phone)
+            user.is_active = True
+            user.save()
+            print(self.style.SUCCESS(f'de_active {user}'))
+        except:
+            raise CommandError('user not find')
--- customer/management/commands/is_active.py
+++ customer/management/commands/is_active.py
@@ -1,16 +1,19 @@
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 from argparse import ArgumentParser
 from core.models import User
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument('-p', '--phone', help='please enter your phone number')
+    def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument('-p', '--phone', help='please enter your phone number', required=True)
 
     def handle(self, *args, **options):
         phone = options['phone']
-        user = User.objects.get(phone=phone)
-        user.is_active = True
-        user.save()
-        print(f'is_active {user}')
-
+        try:
+            user = User.objects.get(phone=phone)
+            user.is_active = True
+            user.save()
+            # print(self.style.SUCCSESS(f'is_active {user}'))
+            self.stdout.write(self.style.SUCCESS(f'is_active {user}'))
+        except:
+            raise CommandError('user not find')
