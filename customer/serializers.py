9bb9419b83577e4bf212c16b111654bce853008e
mohammad <mohammadnasimi6803@gmail.com>
2022-02-26 21:25:58 +0330

    add/ Userserializers for customer info

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 
-from customer.models import Address,Customer
+from customer.models import Address, Customer
 from core.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['user', 'address_set']
-    address_set = serializers.HyperlinkedRelatedField(view_name='customer:Address_Detail',queryset=Address.objects.all(), many=True)
+
+    address_set = serializers.HyperlinkedRelatedField(view_name='customer:Address_Detail',
+                                                      queryset=Address.objects.all(), many=True)
+    user = UserSerializer(read_only=True)
 
 
 class AddressSerializer(serializers.ModelSerializer):
