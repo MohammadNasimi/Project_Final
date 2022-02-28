33f79a0451d75a79aa763ed29b555d321b743175
mohammad <mohammadnasimi6803@gmail.com>
2022-02-28 22:00:03 +0330

    add/add_to_OrderView view class url

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import product_for_categoryListview, ProductDetailView
+from .views import product_for_categoryListview, ProductDetailView,add_to_OrderView
 
 app_name = 'product'
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('product/', productListview.as_view(), name='product'),
     path('category_product/<int:pk>', product_for_categoryListview.as_view(), name='category_product'),
     path('Detail_product/<int:pk>', ProductDetailView.as_view(), name='Detail_product'),
+    path('add_product/<int:pk>', add_to_OrderView.as_view(), name='add_product'),
 ]
