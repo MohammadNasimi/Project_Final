bd3bef76ec2746d2d4f9328fe4109b6025c9ee78
mohammad <mohammadnasimi6803@gmail.com>
2022-03-01 19:53:37 +0330

    add/address update url

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from customer.views import UserlistViewApi ,UserDetailViewApi ,AddressDetailViewApi,AddresslistViewApi
+from customer.views import UserlistViewApi ,UserDetailViewApi ,AddressDetailViewApi,AddresslistViewApi,AddressupdateViewApi
 
 app_name = 'customer'
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('User_Detail/<int:pk>', UserDetailViewApi.as_view(), name='User_Detail'),
     path('Address_list/', AddresslistViewApi.as_view(), name='Address_list'),
     path('Address_Detail/<int:pk>', AddressDetailViewApi.as_view(), name='Address_Detail'),
+    path('Address_update/<int:pk>', AddressupdateViewApi.as_view(), name='Address_update'),
 ]
