bd4d277949f120aee832674658830d9b53220ddf
mohammad <mohammadnasimi6803@gmail.com>
2022-03-01 17:44:04 +0330

    add/address link in serializers order

--- order/serializers.py
+++ order/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from customer.serializers import AddressSerializer
 from product.serializers import ProductSerializer
 from order.models import Order, Order_item
+from customer.models import Address
 
 
 class Order_itemSerializer(serializers.ModelSerializer):
@@ -18,7 +19,9 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = '__all__'
 
-    address = AddressSerializer(read_only=True)
-    order_items = Order_itemSerializer(read_only=True,many=True)
+    # address = AddressSerializer(read_only=True)
+    address = serializers.HyperlinkedRelatedField(view_name='customer:Address_Detail',
+                                                                queryset=Address.objects.all())
+    order_items = Order_itemSerializer(read_only=True, many=True)
     # Order.order_items_set = serializers.HyperlinkedRelatedField(view_name='order:Order_item',
     #                                                          queryset=Order_item.objects.all(), many=True)
