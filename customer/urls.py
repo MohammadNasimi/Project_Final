eebc97f55a924589d602f3db5e40f1b8be23d070
mohammad <mohammadnasimi6803@gmail.com>
2022-03-04 00:17:06 +0330

    create url create_Address

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from customer.views import UserlistViewApi ,UserDetailViewApi ,AddressDetailViewApi,AddresslistViewApi,AddressupdateViewApi
+from customer.views import UserlistViewApi, UserDetailViewApi, AddressDetailViewApi, AddresslistViewApi, \
+    AddresscreateView
 
 app_name = 'customer'
 urlpatterns = [
@@ -8,5 +9,5 @@ urlpatterns = [
     path('User_Detail/<int:pk>', UserDetailViewApi.as_view(), name='User_Detail'),
     path('Address_list/', AddresslistViewApi.as_view(), name='Address_list'),
     path('Address_Detail/<int:pk>', AddressDetailViewApi.as_view(), name='Address_Detail'),
-    path('Address_update/<int:pk>', AddressupdateViewApi.as_view(), name='Address_update'),
+    path('Address_create/', AddresscreateView.as_view(), name='Address_create'),
 ]
