c26c7f3c0d5a3e6e4b91c9667c932724c68ae6b0
mohammad <mohammadnasimi6803@gmail.com>
2022-03-05 00:18:08 +0330

    add/remove url order_items

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from order.views import OrderViewSets,Order_itemViewSets
+from order.views import OrderViewSets, Order_itemViewSets,Order_itemsDeleteView
 
 router = DefaultRouter()
 router.register('Order', OrderViewSets, basename='Order')
@@ -9,4 +9,5 @@ router.register('Order_item', Order_itemViewSets, basename='Order_item')
 app_name = 'order'
 urlpatterns = [
     path('', include(router.urls)),
+    path('delete_order_item/<int:pk>', Order_itemsDeleteView.as_view(),name='delete_order_item'),
 ]
