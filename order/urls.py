ecc4e7ca55f6301f363a5df97ea0dcc2fdc9351f
mohammad <mohammadnasimi6803@gmail.com>
2022-03-07 00:34:29 +0330

    add/url card_list

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from order.views import OrderViewSets, Order_itemViewSets,Order_itemsDeleteView
+from order.views import OrderViewSets, Order_itemViewSets,Order_itemsDeleteView,card_list
 
 router = DefaultRouter()
 router.register('Order', OrderViewSets, basename='Order')
@@ -10,4 +10,5 @@ app_name = 'order'
 urlpatterns = [
     path('', include(router.urls)),
     path('delete_order_item/<int:pk>', Order_itemsDeleteView.as_view(),name='delete_order_item'),
+    path('card_list/', card_list.as_view(), name='card_list'),
 ]
