19e73027dff1240965ec1a1e3e9f2c65c0dba55c
mohammad <mohammadnasimi6803@gmail.com>
2022-03-08 11:13:06 +0330

    split/delete session and Order order_item

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from order.views import OrderViewSets, Order_itemViewSets,Order_itemsDeleteView,card_list,Order_itemsUpdateView
+from order.views import OrderViewSets, Order_itemViewSets, Order_itemsDeleteView, card_list, Order_itemsUpdateView, \
+    Order_items_sessionDeleteView
 
 router = DefaultRouter()
 router.register('Order', OrderViewSets, basename='Order')
@@ -9,7 +10,8 @@ router.register('Order_item', Order_itemViewSets, basename='Order_item')
 app_name = 'order'
 urlpatterns = [
     path('', include(router.urls)),
-    path('delete_order_item/<int:pk>', Order_itemsDeleteView.as_view(),name='delete_order_item'),
-    path('update_order_item/<int:pk>', Order_itemsUpdateView.as_view(),name='update_order_item'),
+    path('delete_order_item/<int:pk>', Order_itemsDeleteView.as_view(), name='delete_order_item'),
+    path('delete_order_item_session/<int:pk>', Order_itemsDeleteView.as_view(), name='delete_order_item_session'),
+    path('update_order_item/<int:pk>', Order_items_sessionDeleteView.as_view(), name='update_order_item'),
     path('card_list/', card_list.as_view(), name='card_list'),
 ]
