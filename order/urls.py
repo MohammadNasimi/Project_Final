f89159488b9673e978e9663939c812aa5576df24
mohammad <mohammadnasimi6803@gmail.com>
2022-03-08 17:14:56 +0330

    add/url card_list_orderView

--- order/urls.py
+++ order/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from order.views import OrderViewSets, Order_itemViewSets, Order_itemsDeleteView, card_list, Order_itemsUpdateView, \
-    Order_items_sessionDeleteView ,Order_itemsUpdate_sessionView
+    Order_items_sessionDeleteView, Order_itemsUpdate_sessionView, card_list_orderView
 
 router = DefaultRouter()
 router.register('Order', OrderViewSets, basename='Order')
@@ -11,9 +11,12 @@ app_name = 'order'
 urlpatterns = [
     path('', include(router.urls)),
     path('delete_order_item/<int:pk>', Order_itemsDeleteView.as_view(), name='delete_order_item'),
-    path('delete_order_item_session/<int:pk>', Order_items_sessionDeleteView.as_view(), name='delete_order_item_session'),
+    path('delete_order_item_session/<int:pk>', Order_items_sessionDeleteView.as_view(),
+         name='delete_order_item_session'),
     path('update_order_item/<int:pk>', Order_items_sessionDeleteView.as_view(), name='update_order_item'),
     path('update_order_item/<int:pk>', Order_itemsUpdateView.as_view(), name='update_order_item'),
-    path('update_order_item_session/<int:pk>', Order_itemsUpdate_sessionView.as_view(), name='update_order_item_session'),
+    path('update_order_item_session/<int:pk>', Order_itemsUpdate_sessionView.as_view(),
+         name='update_order_item_session'),
     path('card_list/', card_list.as_view(), name='card_list'),
+    path('card_list_order/', card_list_orderView.as_view(), name='card_list_order'),
 ]
