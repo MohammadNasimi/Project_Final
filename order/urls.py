2d29e07346ec968c7c0a02b69adf86e2050a8c97
mohammad <mohammadnasimi6803@gmail.com>
2022-03-08 11:45:27 +0330

    add/update list session order_item url

--- order/urls.py
+++ order/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from order.views import OrderViewSets, Order_itemViewSets, Order_itemsDeleteView, card_list, Order_itemsUpdateView, \
-    Order_items_sessionDeleteView
+    Order_items_sessionDeleteView ,Order_itemsUpdate_sessionView
 
 router = DefaultRouter()
 router.register('Order', OrderViewSets, basename='Order')
@@ -13,5 +13,7 @@ urlpatterns = [
     path('delete_order_item/<int:pk>', Order_itemsDeleteView.as_view(), name='delete_order_item'),
     path('delete_order_item_session/<int:pk>', Order_items_sessionDeleteView.as_view(), name='delete_order_item_session'),
     path('update_order_item/<int:pk>', Order_items_sessionDeleteView.as_view(), name='update_order_item'),
+    path('update_order_item/<int:pk>', Order_itemsUpdateView.as_view(), name='update_order_item'),
+    path('update_order_item_session/<int:pk>', Order_itemsUpdate_sessionView.as_view(), name='update_order_item_session'),
     path('card_list/', card_list.as_view(), name='card_list'),
 ]
